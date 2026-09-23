61acd05bc21d09404134141b8250e14d90c89069
theshoxjaxon <theshoxjaxon@gmail.com>
2026-09-23 16:49:35 +0500

    node js 2 dars

---
+++ app.js
@@ -0,0 +1,11 @@
+import fs from "fs"
+import os from "node:os"
+import chalk from "chalk"
+
+import { qosh, ayir, kopaytir, bol } from "./calculator.js"
+console.log(bol(5, 5))
+console.log("salom")
+
+
+console.log(chalk.green("Tollka Trenerovka"))
+
---
+++ calculator.js
@@ -0,0 +1,4 @@
+export const qosh = (a, b) => a + b;
+export const ayir = (a, b) => a - b;
+export const kopaytir = (a, b) => a * b;
+export const bol = (a, b) => a / b;  
