798fcf4ea1f107bb45453f282e38875600416fa3
theshoxjaxon <theshoxjaxon@gmail.com>
2026-09-25 17:10:19 +0500

    added 2 day in node js

---
+++ day2/order.js
@@ -0,0 +1,19 @@
+// console.log("A");
+// setTimeout(() => console.log("B"), 0);
+// Promise.resolve().then(() => console.log("C"));
+// setTimeout(() => console.log("D"), 0);
+// console.log("E");
+
+// // A, E, C, ?? D || B  &&  B || D 
+
+
+// console.log("Start");
+// setTimeout(() => console.log("Salom"), 0);
+// const boshlandi = Date.now();
+// while (Date.now() - boshlandi < 2000) { }   // 2 soniya qulflash
+// console.log(`Blok tugadi (${Date.now() - boshlandi}ms)`);
+
+
+// qizil -  40s
+//  sariq -  20s
+// yashil - 10s
